40edcf1b1d30309952651e8fa0d1344ad2eef002
Aswin-S-T <aswinswebart15@gmail.com>
2024-02-26 15:49:08 +0530

    new changes

--- frontend/src/Constants/urls.js
+++ frontend/src/Constants/urls.js
@@ -6,3 +6,6 @@ export const BANNER_VIDEO =
 
 export const BRAND_ICON =
   "https://s3-alpha-sig.figma.com/img/2efb/d2df/887a0c4388ef2816f7d81d2cb5aea050?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YociuCZEupXKl816dYdePpLGDHyPYyrofwGocrsZA~ff1YSdxSSeMdz~wnjjdA5OdRu6DQZN~EPSHz6txvoZUANHKKRwYYbW-Q675N3X-56lxY2MszczeOp7RqucWL9e3UpJUGzSymgz8cmxqo9qtn5bXyZvpv31jfzu5GoKY9xHl~EFZPbKWV~dVhG1SM77qgdkDvdset1V9t6AUctNeZ4GXIIrSvbegOuuIINPWTKCw~vhoJ-1sxKlwF7UWgkYP~d3-Tip7kpcFYgbZEkerlnqB2X8DGmL1MVgt3TusXuUWyDJuPoawPXSTNMFCiEMZ9yCpi4tdM6iebFAAURHdQ__";
+
+export const CARD_IMAGE =
+  "https://s3-alpha-sig.figma.com/img/1a38/56d2/b6dc9c6a880c2dadf15014ad81ae0163?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iCKBHqxhJwo5BGhccQuc5KCPq3nvLbH6FtGTsDzDKlCnBqgHp-Etrg1ds~MkkUqhylUpdVtdZZVMMUZb4QhusK5PbP8Abg4csGMehcPHEEDLY1-UG-~j7n73jTSfzeVn9PVQyKr3tqRSpUP4c3uCntjpgNdqZcrDt8ARLTkAjJUAB6K21X3vB5ikHDEkjC8ALCBmaXhOfgHqODXNAmjfZsKy~g~5CVrSX4bwBxduCW9SAY0AM6TfxJI3~SF9GWRL-sQJTmi8kpFmqn4mHf2xK~UmFJLdnSmrOzz15YVPwX0o-KNfILjP8ni9XBHBwX-f~bLcZVAePuL-N-HPvxGQtw__";
