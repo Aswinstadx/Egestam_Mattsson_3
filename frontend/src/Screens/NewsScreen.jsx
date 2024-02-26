3165d608db749729d1b7b5533b5f562b195c4f9c
Aswin-S-T <aswinswebart15@gmail.com>
2024-02-26 15:22:17 +0530

    small changes done

--- frontend/src/Screens/NewsScreen.jsx
+++ frontend/src/Screens/NewsScreen.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import NewsBanner from "../Components/News/NewsBanner";
+import Card from "../Components/Card/Card";
 
 function NewsScreen() {
   return (
     <div>
       <NewsBanner />
+      <div className="container">
+        <div className="card-container mt-5">
+          {[...Array(9)].map((_, index) => (
+            <Card key={index} title={`Card ${index + 1}`} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
