b9b93fb213527bc7114b478f28d4129c12a1caeb
Aswin-S-T <aswinswebart15@gmail.com>
2024-03-02 15:49:22 +0530

    responsiveness for mobile and ipad view is done on news page

--- frontend/src/Components/News/NewsBanner.jsx
+++ frontend/src/Components/News/NewsBanner.jsx
@@ -5,7 +5,6 @@ function NewsBanner() {
   return (
     <div>
       <div className="banner-container">
-        <div className="banner-overlay" />
         <ReactPlayer
           url="https://s3-figma-videos-production-sig.figma.com/video/1309623234516428457/TEAM/2158/e648/-1250-4054-b1f3-6cf60369b5c8?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Q7MgzIZ1smyd8cMOSVYiipPdsz2djXOQaZoprTghftTtGLmZ5TbExnibVw8wwsiDuefDbxiRY7Vd~gqvmxa4MB6DLHj7~4HkxMh7io2agRm4Z4v4AmDQT9CVgFLTwBGZuaxYvShVGtBjXIZEsf~BauOzR3zbe7-DIWyEuqwGMvBRx-IHr-9gP6hqO-aYUa1X0JJ9yp3VOqrcCtmLMLx30hmQe9ZvzxRgc67D1h9RBE2Ef~ZofHyX3aG-bUEj58Eg4sVzKy0DpnDMHiT2G6dpwUPH~1mB4bxdMpVVgMRHLT8SqkrgOqo73Nlt2IpFWOvNz5OhH5qYQu39PhFUrad~~Q__"
           playing={true}
@@ -27,6 +26,27 @@ function NewsBanner() {
             annan övrig information.
           </p>
         </div>
+        {/* <ReactPlayer
+          url="https://s3-figma-videos-production-sig.figma.com/video/1309623234516428457/TEAM/2158/e648/-1250-4054-b1f3-6cf60369b5c8?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Q7MgzIZ1smyd8cMOSVYiipPdsz2djXOQaZoprTghftTtGLmZ5TbExnibVw8wwsiDuefDbxiRY7Vd~gqvmxa4MB6DLHj7~4HkxMh7io2agRm4Z4v4AmDQT9CVgFLTwBGZuaxYvShVGtBjXIZEsf~BauOzR3zbe7-DIWyEuqwGMvBRx-IHr-9gP6hqO-aYUa1X0JJ9yp3VOqrcCtmLMLx30hmQe9ZvzxRgc67D1h9RBE2Ef~ZofHyX3aG-bUEj58Eg4sVzKy0DpnDMHiT2G6dpwUPH~1mB4bxdMpVVgMRHLT8SqkrgOqo73Nlt2IpFWOvNz5OhH5qYQu39PhFUrad~~Q__"
+          playing={true}
+          playsinline
+          loop={true}
+          volume={0}
+          controls={false}
+          width="100vw"
+          // height="599px"
+          height="auto"
+          className="player"
+        />
+        <div className="banner-text2">
+          <h1>Nyheter</h1>
+          <p>
+            Här uppdaterar vi regelbundet och publicerar välresearchade
+            artiklar, tips för energibesparing och säkerhetsrekommendationer. Du
+            hittar även nyheter om vad som händer hos oss med nya projekt och
+            annan övrig information.
+          </p>
+        </div> */}
       </div>
     </div>
   );
--- frontend/src/Screens/NewsScreen.jsx
+++ frontend/src/Screens/NewsScreen.jsx
@@ -6,6 +6,7 @@ import MobileFooter from "../Components/Footer/MobileFooter";
 
 function NewsScreen() {
   const [isMobile, setIsMobile] = useState(false);
+  
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
@@ -22,7 +23,7 @@ function NewsScreen() {
   return (
     <div>
       <NewsBanner />
-      <div className="container">
+      {/* <div className="container">
         {!isMobile && (
           <>
             <select
@@ -45,7 +46,7 @@ function NewsScreen() {
         <div className="" style={{ float: "right" }}>
           <select
             className="mt-5 card-select"
-            style={{ top: "22px", position: "relative", float: "right" }}
+            style={{ top: !isMobile && "22px", position: "relative", float: "right" }}
           >
             <option>Visa fler</option>
             <option>Kategori</option>
@@ -58,7 +59,7 @@ function NewsScreen() {
       {isMobile && (
         <footer
           style={{
-            top: isMobile && "250px",
+            top: isMobile && "50px",
             position: "relative",
             width: "100%",
           }}
@@ -75,6 +76,7 @@ function NewsScreen() {
           <Footer />
         </footer>
       )}
+    </div> */}
     </div>
   );
 }
