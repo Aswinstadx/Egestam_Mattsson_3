ba64eb20a4991283c97428213883b560803f519d
Aswin-S-T <aswinswebart15@gmail.com>
2024-03-05 12:02:51 +0530

    Releaterat section is not needed in the mac and also the project section is missing in the mac.

--- frontend/src/Screens/NewsDetailsScreen.jsx
+++ frontend/src/Screens/NewsDetailsScreen.jsx
@@ -6,10 +6,11 @@ import RelatedNews from "../Sections/RelatedNews";
 import Section6 from "../Components/News/Section6";
 import MobileOnlySust from "../Components/Sustainability/MobileOnlySust";
 import Section1 from "../Sections/Section1/Section1";
+import News from "../Components/News/News";
 
 function NewsDetailsScreen() {
   const [isMobile, setIsMobile] = useState(false);
-
+  const [isIpad, setIsIpad] = useState(false);
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
@@ -23,6 +24,24 @@ function NewsDetailsScreen() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsIpad(window.innerWidth <= 1024 && window.innerWidth >= 600); // Assuming iPad width is less than or equal to 768px
+    };
+
+    // Initial check
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <Banner />
@@ -152,8 +171,18 @@ function NewsDetailsScreen() {
         className="related-news-section-6-div"
         style={{ backgroundColor: isMobile && "#EFF6E7" }}
       >
-        <RelatedNews />
-        <Section6 />
+        {isIpad ? (
+          <>
+            <News />
+          </>
+        ) : (
+          <>
+            <RelatedNews />
+            <Section6 />
+          </>
+        )}
+
+        {isIpad && <>{console.log("This is ipad")}</>}
       </div>
       {isMobile && (
         <div style={{ marginTop: "10px" }}>
